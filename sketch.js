b32d566176c0e688f7ea507cedcdf774e1e0f9bd
maitri-bothra <73102658+maitri-bothra@users.noreply.github.com>
2020-11-09 22:12:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function setup() {
+  createCanvas(480,800);
+  ground = new Ground(240,height,480,20);
+  
+
+}
+
+function draw() {
+  background("black");  
+  
+  ground.display();
+}
